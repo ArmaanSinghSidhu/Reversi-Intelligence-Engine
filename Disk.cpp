865b5ea2145bd67990adcda45b3d7bcacae57f6b
DareMo314 <picklepie3.141@gmail.com>
2026-06-15 13:45:02 -0700

    Update Disk.cpp

--- Disk.cpp
+++ Disk.cpp
@@ -1,1 +1,35 @@
+#ifndef DISK_H
+#define DISK_H
 
+class Disk {
+    private:
+    char color; //B, W, .
+    public:
+    Disk() : color('.') {}//. equals empty
+
+    Disk(char c) {
+        color = c;
+    }
+    char get_color() const {
+        return color;
+    }
+    void set_color(char c) {
+        color = c;
+    }
+    bool is_empty() const {
+        return color == '.';
+    }
+    void flip() {
+        if (color == 'B'){
+            set_color('W');
+        }
+        else if (color == 'W') {
+            set_color('B');
+        }
+    }
+    bool operator==(const Disk& d) const{
+        return color == d.color;
+    }
+};
+
+#endif
